64592d8de5af6d65e7bf6cfddc1f1f5ffe7ed6ba
smetanin <smetanin@mail.ru>
2020-04-24 14:49:53 +0300

    final test 1.h

---
+++ test1.h
@@ -0,0 +1,11 @@
+#ifndef TEST1_H_INCLUDED
+#define TEST1_H_INCLUDED
+#include <iostream>
+#include <vector>
+
+void test_positive();
+void test_similar ();
+void test_negative();
+void test_one_number();
+void test_empty();
+#endif // TEST1_H_INCLUDED
