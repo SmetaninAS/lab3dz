4ef13103dedc4a601d5365e1909e089e300754ac
smetanin <smetanin@mail.ru>
2020-04-24 14:46:17 +0300

    test h

---
+++ test.h
@@ -0,0 +1,14 @@
+#ifndef TEST_H_INCLUDED
+#define TEST_H_INCLUDED
+#include <iostream>
+#include <vector>
+using namespace std;
+void test_positive();
+void test_similar ();
+void test_negative();
+void test_one_number();
+void test_empty();
+
+
+
+#endif // TEST_H_INCLUDED
